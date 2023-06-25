85c88e2d4de8bb483d9812145c9aacfd4193edb8
Revixa1 <txguimont@hotmail.com>
2023-06-25 13:16:58 -0400

    okok

--- couche.cpp
+++ couche.cpp
@@ -9,3 +9,78 @@
 #include "couche.h"
 
 // Implementation a faire...
+
+
+
+Couche::Couche()
+{
+    bool initialise=1;
+    bool active=0;
+    bool inactive=0;
+    vec.cleanVecteur();
+
+}
+
+
+Couche::~Couche()
+{
+
+}
+
+
+
+bool ajouterForme()
+{
+
+
+}
+
+
+int retirerForme(int indexeIn)
+{
+
+
+}
+int obtenirForme(int indexeIn)
+{
+
+}
+double aireTotale()
+{
+
+}
+bool translaterCouche(int Dx, int Dy)
+{
+
+}
+bool initCouche()
+{
+
+}
+
+
+bool setEtatCouche(int EtatIn)
+{
+    if(EtatIn==0)
+    {
+        initialise=1;
+        active=0;
+        inactive=0;
+    } else if(EtatIn==1)
+    {
+        initialise=0;
+        active=1;
+        inactive=0;
+    } else if(EtatIn==2)
+    {
+        initialise=0;
+        active=0;
+        inactive=1;
+    } else 
+    {
+        return false;
+    }
+
+    return true;
+}
+
--- couche.h
+++ couche.h
@@ -11,9 +11,35 @@
 #ifndef COUCHE_H
 #define COUCHE_H
 #include "vecteur.h"
+#include "forme.h"
+#include "rectangle.h"
 class Couche
 {
    // Classe a completer
+   public:
+      Couche();
+      ~Couche();
+
+
+      bool ajouterForme();
+      int retirerForme(int indexeIn);
+      int obtenirForme(int indexeIn);
+      double aireTotale();
+      bool translaterCouche(int Dx, int Dy);
+      bool initCouche();
+      bool setEtatCouche(int EtatIn);
+
+
+   private:
+      bool initialise;
+      bool active;
+      bool inactive;
+      
+     
+
+     
+
+
 };
 
 #endif
